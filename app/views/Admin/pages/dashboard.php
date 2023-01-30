1d34522236875d9470d1f0320681b2108370f44f
widya yasa <widyarusmananda15@gmail.com>
2023-01-30 23:02:51 +0800

    dashboard page done

--- app/views/Admin/pages/dashboard.php
+++ app/views/Admin/pages/dashboard.php
@@ -19,8 +19,8 @@
                         <div class="card card-dashboard-wrapper">
                             <div class="card-body card-dashboard-content d-flex flex-row justify-content-between">
                                 <div class="card-dashboard-name d-flex flex-column p-2">
-                                    <p class="card-dashboard-desc ">Total Activities</p>
-                                    <p class="card-dashboard-value fs-3 fw-bold ">09</p>
+                                    <p class="card-dashboard-desc ">Total Citizens</p>
+                                    <p class="card-dashboard-value fs-3 fw-bold "><?=$data['countCitizen']?> <span class="fs-6">Citizens</span></p>
                                 </div>
                                 <div class="img-card-dashboard-wrapper">
                                     <img src="{{asset('img/dashboard/total-activity.svg')}}" class="img-fluid" alt="">
@@ -32,8 +32,8 @@
                         <div class="card card-dashboard-wrapper">
                             <div class="card-body card-dashboard-content d-flex flex-row justify-content-between">
                                 <div class="card-dashboard-name d-flex flex-column p-2">
-                                    <p class="card-dashboard-desc ">Activities Finished</p>
-                                    <p class="card-dashboard-value fs-3 fw-bold ">04</p>
+                                    <p class="card-dashboard-desc ">Total Critics</p>
+                                    <p class="card-dashboard-value fs-3 fw-bold "><?=$data['countCritics']?> <span class="fs-6">Critics</span></p>
                                 </div>
                                 <div class="img-card-dashboard-wrapper">
                                     <img src="{{asset('img/dashboard/activity-finished.svg')}}" class="img-fluid" alt="">
@@ -45,8 +45,8 @@
                         <div class="card card-dashboard-wrapper">
                             <div class="card-body card-dashboard-content d-flex flex-row justify-content-between">
                                 <div class="card-dashboard-name d-flex flex-column p-2">
-                                    <p class="card-dashboard-desc ">Total Income</p>
-                                    <p class="card-dashboard-value fs-3 fw-bold ">IDR. 500.000</p>
+                                    <p class="card-dashboard-desc ">Total Feedback</p>
+                                    <p class="card-dashboard-value fs-3 fw-bold "><?=$data['countFeedback']?> <span class="fs-6">Critics</span></p>
                                 </div>
                                 <div class="img-card-dashboard-wrapper">
                                     <img src="{{asset('img/dashboard/total-income.svg')}}" class="img-fluid" alt="">
@@ -58,8 +58,8 @@
                         <div class="card card-dashboard-wrapper">
                             <div class="card-body card-dashboard-content d-flex flex-row justify-content-between">
                                 <div class="card-dashboard-name d-flex flex-column p-2">
-                                    <p class="card-dashboard-desc ">Total Target</p>
-                                    <p class="card-dashboard-value fs-3 fw-bold ">124</p>
+                                    <p class="card-dashboard-desc ">Unfeedback Critics</p>
+                                    <p class="card-dashboard-value fs-3 fw-bold "><?=$data['countUnFeedbackCritic']?> <span class="fs-6">Critics</span></p>
                                 </div>
                                 <div class="img-card-dashboard-wrapper">
                                     <img src="{{asset('img/dashboard/total-target.svg')}}" class="img-fluid" alt="">
@@ -69,69 +69,21 @@
                     </div>
                 </div>
                 <div class="mid-main-content-dashboard row gy-4 pb-5 pt-4">
-                    <div class="col-lg-6 col-12 ">
+                    <div class="col-12">
                         <div class="card card-chart">
                             <div class="card-body d-flex flex-column">
-                                <div class="d-flex flex-row chart-header justify-content-between">
-                                    <p class="fs-4 fw-semibold">Activity Performance</p>
-                                    <div class="dropdown">
-                                        <button class="btn btn-dropdown dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Last 1 Month
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item main-color" href="#">Action</a></li>
-                                            <li><a class="dropdown-item main-color" href="#">Another action</a></li>
-                                            <li><a class="dropdown-item main-color" href="#">Something else here</a></li>
-                                        </ul>
+                                <div class="d-flex flex-column chart-header">
+                                    <p class="display-4">Selamat Datang Admin</p>
+                                    <p class="fs-3 pt-3">Klik Tombol dibawah untuk melihat keluhan terbaru</p>
+                                    <div class="pt-3">
+                                        <a href="<?=BASEURL?>critics" class="btn btn-color">See Critics</a>
                                     </div>
+
+
                                 </div>
                             </div>
-                            <div class="pt-4 px-3">
-                                <canvas class="pt-4" id="myChart"></canvas>
-                            </div>
+
                         </div>
-
-
-                    </div>
-                    <div class="col-lg-6 col-12 ">
-                        <div class="card card-chart">
-                            <div class="card-body d-flex flex-column">
-                                <div class="d-flex flex-row chart-header justify-content-between">
-                                    <p class="fs-4 fw-semibold">Target Activity  Today</p>
-                                </div>
-                                <div class="table-dashboard pt-4 px-3">
-                                    <div class="top-table row px-2 py-3">
-                                        <div class="col-lg-2 col-md-2 d-lg-flex d-md-flex d-none">No</div>
-                                        <div class="col-lg-6 col-md-6 col-6">Activity</div>
-                                        <div class="col-lg-4 col-md-4 col-6">Level</div>
-                                    </div>
-                                    <div class="content-table-grup ">
-                                        <div class="content-table row px-2 py-3">
-                                            <div class="col-lg-2 col-md-2 d-lg-flex d-md-flex d-none">1.</div>
-                                            <div class="col-lg-6 col-md-6 col-6">Landing Barbershop</div>
-                                            <div class="col-lg-4 col-md-4 col-6">Medium</div>
-                                        </div>
-                                        <div class="content-table row px-2 py-3">
-                                            <div class="col-lg-2 col-md-2 d-lg-flex d-md-flex d-none">2.</div>
-                                            <div class="col-lg-6 col-md-6 col-6">Web System Vote</div>
-                                            <div class="col-lg-4 col-md-4 col-6">Hard</div>
-                                        </div>
-                                        <div class="content-table row px-2 py-3">
-                                            <div class="col-lg-2 col-md-2 d-lg-flex d-md-flex d-none">3.</div>
-                                            <div class="col-lg-6 col-md-6 col-6">Study Vue</div>
-                                            <div class="col-lg-4 col-md-4 col-6">Hard</div>
-                                        </div>
-                                        <div class="content-table row px-2 py-3">
-                                            <div class="col-lg-2 col-md-2 d-lg-flex d-md-flex d-none">4.</div>
-                                            <div class="col-lg-6 col-md-6 col-6">Build Logo</div>
-                                            <div class="col-lg-4 col-md-4 col-6">Medium</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
                     </div>
                 </div>
             </div>
